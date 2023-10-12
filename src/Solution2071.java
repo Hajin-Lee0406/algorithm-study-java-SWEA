5a5fd7c503ab1ad34b2de147a3dfbe4197079f69
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-12 11:26:41 +0900

    2071. 평균값 구하기(D1)

---
+++ src/Solution2071.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution2071 {
+    public static void main(String args[]) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine()); // 테스트 케이스 개수
+        StringTokenizer st;
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            // 입력값 모두 더하기
+            st = new StringTokenizer(reader.readLine(), " ");
+
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++) {
+                sum = sum + Integer.parseInt(st.nextToken());
+            }
+
+            // 평균 구하기
+            int average = Math.round((float) sum / 10);
+
+            System.out.println("#" + test_case + " " + average);
+        }
+    }
+}
