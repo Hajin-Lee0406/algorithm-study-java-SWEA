e8b11aff724877eb5ff7a9c041a0f76df4c64477
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-27 22:57:34 +0900

    1284. 수도 요금 경쟁 (D2)

---
+++ src/P1_2023_10/Solution1945.java
@@ -0,0 +1,17 @@
+package P1_2023_10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution1945 {
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        int T = Integer.parseInt(reader.readLine()); // 테스트 케이스 개수
+//
+//        for (int test_case = 1; test_case <= T; test_case++) {
+//            int input = Integer.parseInt(reader.readLine());
+//
+//            System.out.println("#" + test_case + " " + result);
+//        }
+//    }
+}
